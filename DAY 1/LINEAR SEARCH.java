ad82de7d15184c3a6db1ce52c184ed05250a6118
Tarun <tarunar006@gmail.com>
2026-09-10 15:24:11 +0530

    Add linear search implementation in Java

---
+++ DAY 1/LINEAR SEARCH.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 30, 45, 50};
+        int target = 30;
+
+        int index = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1)
+            System.out.println("Element found at index " + index);
+        else
+            System.out.println("Element not found");
+    }
+}
