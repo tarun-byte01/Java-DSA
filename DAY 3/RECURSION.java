58e6cc12a8da004260edd998f1fd485266e2d9c0
Tarun <tarunar006@gmail.com>
2026-09-12 21:43:15 +0530

    Add recursive print function in RECURSION.java
    Implement a recursive function to print numbers from n to 1.

---
+++ DAY 3/RECURSION.java
@@ -0,0 +1,23 @@
+class Main {
+    static void print(int n) {
+
+        if (n == 0) {
+            return;
+        }
+
+        System.out.println(n);
+
+        print(n - 1);
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
+
+Output
+5
+4
+3
+2
+1
